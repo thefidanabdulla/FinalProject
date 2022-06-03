2cb8b0ae7d61ecf1cb025cc588ae7a659d790828
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-03 21:46:06 +0400

    Camp 10. Day First Break

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +18,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new Result(true, "Product Added");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -25,6 +32,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get( p => p.ProductId == productId );
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
